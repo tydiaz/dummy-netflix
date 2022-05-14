3449991a31b896d5c242ef8cc959f3a613c239e4
Ty <nodedev01@protonmail.com>
2022-05-14 17:40:24 -0400

    add banner component and banner styles

--- components/banner/banner.component.js
+++ components/banner/banner.component.js
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import styles from './banner.module.css';
+
 const Banner = (props) => {
   const { title, subTitle, imageUrl } = props;
 
@@ -6,18 +9,32 @@ const Banner = (props) => {
   };
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.seriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subtitle}>{subTitle}</h3>
+          <div className={styles.buttonWrapper}>
+            <button onClick={handleOnPlay} className={styles.button}>
+              <Image
+                src='/static/play_arrow.svg'
+                alt='play button icon'
+                width='32px'
+                height='32px'
+              />
+              <span className={styles.buttonText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImage}
         style={{
           backgroundImage: `url(${imageUrl})`,
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          backgroundSize: 'cover',
-          backgroundPosition: '50% 50%',
         }}
       ></div>
     </div>
--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin='anonymous'
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap'
             rel='stylesheet'
           />
         </Head>
