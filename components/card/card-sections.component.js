21fe7008b138a8bb065e66c628bbe9c82a3210fe
Ty <nodedev01@protonmail.com>
2022-06-01 20:24:21 -0400

    add placeholder data and vids library that calls youtube api

--- components/card/card-sections.component.js
+++ components/card/card-sections.component.js
@@ -2,15 +2,15 @@ import Card from './card.component';
 import styles from './card-sections.module.css';
 
 const CardSections = (props) => {
-  const { title, movies, size } = props;
-  console.log({ movies });
+  const { title, vids = [], size } = props;
+  console.log({ vids });
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
-        {movies.map((movie, idx) => {
+        {vids.map((video, idx) => {
           return (
-            <Card id={idx} key={idx} imageUrl={movie.imageUrl} size={size} />
+            <Card id={idx} key={idx} imageUrl={video.imageUrl} size={size} />
           );
         })}
       </div>
